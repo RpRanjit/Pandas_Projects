4d6ef1116c894f59fd0cb2ec73aa246016fb9fdd
Ranjit Poudel <ranjitsparrow2@gmail.com>
2026-02-16 21:53:59 +0545

    adding second part

--- Project-6/index.py
+++ Project-6/index.py
@@ -23,7 +23,7 @@ print(data.info())
 # 5. Check Null Values In The Dataset
 
 print(data.isnull().sum())
-sns.heatmap(data.isnull())
+# sns.heatmap(data.isnull())
 # plt.show()
 # 6. Check For Duplicate Data and Drop Them
 
@@ -34,14 +34,14 @@ print(data.duplicated().sum())
 # 7. Find Out Number of Courses Per Subjects
 print(data.columns)
 print(data['subject'].value_counts())
-sns.countplot(data["subject"])
+# sns.countplot(data["subject"])
 # plt.show()
 
 # 8. For Which Levels, Udemy Courses Providing The Courses
 print(data.head(1))
 print(data['level'].value_counts())
 
-sns.countplot(data["level"], palette='Set1')
+# sns.countplot(data["level"], palette='Set1')
 # plt.tight_layout()
 # plt.show()
 
@@ -64,9 +64,35 @@ print(data.groupby('level')['num_subscribers'].sum())
 print(data.columns)
 print(data[data['num_subscribers'].max() == data['num_subscribers']])
 # 14. Display 10 Most Popular Courses As Per Number of Subscribers
+print(data.sort_values(by='num_subscribers', ascending=False).head(10))
+top_10 = data.sort_values(by='num_subscribers', ascending=False).head(10)
+# plt.bar(top_10['course_title'], top_10['num_subscribers'])
+# plt.xlabel("Number of Subscribers")
+# plt.ylabel("Course Title")
+# plt.tight_layout()
+# plt.show()
+
 # 15. Find The Course Which Is Having The Highest Number of Reviews.
+print(data.sort_values(by="num_reviews", ascending=False).head(1))
+
 # 16. Does Price Affect the Number of Reviews?
+# plt.figure(figsize=(15,6))
+# sns.scatterplot(x="price", y="num_reviews", data=data)
+# plt.show()
 # 17. Find Total Number of Courses Related To Python
+print(data.columns)
+print(len(data[data['course_title'].str.contains("python",case=False)]))
+
 # 18. Display 10 Most Popular Python Courses As Per Number of Subscribers
+pyhton_course = data[data['course_title'].str.contains('python', case=False)]
+print(pyhton_course)
+print(pyhton_course.sort_values(by='num_subscribers',ascending=False).head(10))
+# sns.barplot(x='num_subscribers', y='course_title', data=pyhton_course)
+# plt.show()
 # 19. In Which Year The Highest Number of Courses Were Posted?
+print(data.columns)
+data['year'] = data['published_timestamp'].dt.year
+sns.countplot(x='year', data=data)
+plt.show()
 # 20. Display Category-Wise Count of Posted Subjects [Year Wise] 
+print(data.groupby('year')['subject'].value_counts())
